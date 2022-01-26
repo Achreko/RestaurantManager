e38b720dce93414577c3103a984640262c596cb5
Achreko <achrekv3@gmail.com>
2022-01-26 23:07:38 +0100

    Added commands for Restaurant.

---
+++ RestaurantManager.Infrastructure/Commands/CreateRestaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Infrastructure.Commands
+{
+   public class CreateRestaurant
+    {
+        public string Name { get; set; }
+
+        public int NumberOfEmployees { get; set; }
+
+        public DateTime DateOfOpening { get; set; }
+    }
+}
---
+++ RestaurantManager.Infrastructure/Commands/UpdateRestaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Infrastructure.Commands
+{
+    public class UpdateRestaurant
+    {
+        public string Name { get; set; }
+
+        public int NumberOfEmployees { get; set; }
+
+        public double MonthlyRevenue { get; set; }
+    }
+}
