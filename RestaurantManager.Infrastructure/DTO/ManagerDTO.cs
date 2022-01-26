e1c51357524c39002e5f47969026f38f6239ad01
Achreko <achrekv3@gmail.com>
2022-01-26 23:06:07 +0100

    Added DTO for Manager and Restaurant.

---
+++ RestaurantManager.Infrastructure/DTO/ManagerDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Infrastructure.DTO
+{
+    public class ManagerDTO
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public string ForeName { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
---
+++ RestaurantManager.Infrastructure/DTO/RestaurantDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Infrastructure.DTO
+{
+    public class RestaurantDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumberOfEmployees { get; set; }
+
+        public DateTime DateOfOpening { get; set; }
+
+        public double MonthlyRevenue { get; set; }
+    }
+}
